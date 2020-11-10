067be0d59b466482664b640b6bb5671065cebc35
Rolando Penagos <rpmacal2013@outlook.com>
2020-11-09 21:59:36 -0600

    Se arregla el problema de la copia de los dll de sqllite (interop)

--- SQLiteSample1/Program.cs
+++ SQLiteSample1/Program.cs
@@ -16,7 +16,7 @@ namespace SQLiteSample1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Empleados());
         }
     }
 }
